44d0b8ff41fc8ec4572dd9df929fc5b69cb7e998
Sebastien Savater <ssavater@mon-territoire.fr>
2022-04-09 18:08:57 +0200

    Setup TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './app/views/**/*.html.erb',
+    './app/views/**/*.html.slim',
+    './app/helpers/**/*.rb',
+    './app/components/**/*.html.erb',
+    './app/components/**/*.html.slim',
+    './app/components/**/*.rb',
+    './app/javascript/**/*.js'
+  ]
+}
