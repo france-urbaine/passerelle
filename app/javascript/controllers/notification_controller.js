868efa7ee9cf99f82f3f23591bc18d06d9ea4b46
Sebastien Savater <ssavater@mon-territoire.fr>
2022-04-23 11:00:45 +0200

    Update notifications UI

---
+++ app/javascript/controllers/notification_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "@hotwired/stimulus"
+import { useTransition } from "stimulus-use"
+
+export default class extends Controller {
+  static values = {
+    delay: Number
+  }
+
+  connect() {
+    useTransition(this)
+    this.enter()
+    if (this.delayValue) this.timeout = setTimeout(this.hide.bind(this), this.delayValue)
+  }
+
+  async hide () {
+    if (this.timeout) clearTimeout(this.timeout)
+    await this.leave()
+    this.element.remove()
+  }
+}
