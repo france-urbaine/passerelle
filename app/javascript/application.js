a376f4ca8dda09671e81425af44d8fa0cdd3e8f8
Sebastien Savater <ssavater@mon-territoire.fr>
2023-05-03 08:57:27 +0200

    Monkey patch turbo to send search form into proper turbo frame

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,13 +2,26 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 
-// FIXME: Waiting for https://github.com/hotwired/turbo-rails/pull/367 to be merged.
-// It would allow to redirect the full page after submitting a form from a modal.
-//
-// This is a workaround fix from
-// https://github.com/hotwired/turbo/pull/863#issuecomment-1470184953
+// FIXME: Monkeypatch to allow to redirect the full page after submitting a form from a modal.
+// see:
+//  https://github.com/hotwired/turbo-rails/pull/367
+//  https://github.com/hotwired/turbo/pull/863#issuecomment-1470184953
 //
 document.addEventListener("turbo:frame-missing", function(event) {
-  event.preventDefault()
-  event.detail.visit(event.detail.response)
+  if (event.detail.response.redirected) {
+    event.preventDefault()
+    event.detail.visit(event.detail.response)
+  }
+})
+
+// FIXME: Monkeypatch to fix turbo issue of wrong turbo-frame
+// see: https://github.com/hotwired/turbo/pull/579
+//
+document.addEventListener('turbo:before-fetch-request', (event) => {
+  const targetTurboFrame = event.target.getAttribute('data-turbo-frame')
+  const fetchTurboFrame = event.detail.fetchOptions.headers['Turbo-Frame']
+
+  if (targetTurboFrame && targetTurboFrame != fetchTurboFrame && document.querySelector(`turbo-frame#${targetTurboFrame}`)) {
+    event.detail.fetchOptions.headers['Turbo-Frame'] = targetTurboFrame;
+  }
 })
