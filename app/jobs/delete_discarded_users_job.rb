83ca6f1428c1918ef0b8d80711d291c9490f4fbf
Sebastien Savater <ssavater@mon-territoire.fr>
2023-05-09 10:09:16 +0200

    Remove DeleteDiscardedUsersJob, use DeleteDiscardedRecordsJob instead

--- app/jobs/delete_discarded_users_job.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class DeleteDiscardedUsersJob < ApplicationJob
-  queue_as :default
-
-  def perform(*ids)
-    User.discarded.where(id: ids).destroy_all
-  end
-end
