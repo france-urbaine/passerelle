de6893427b4cc481c20484f7eff8f3d8ee45815b
Sebastien Savater <ssavater@mon-territoire.fr>
2023-05-16 09:40:11 +0200

    Enhance flash notifications relative to devise

--- app/controllers/account/sessions_controller.rb
+++ app/controllers/account/sessions_controller.rb
@@ -3,5 +3,14 @@
 module Account
   class SessionsController < Devise::SessionsController
     layout "public"
+
+    private
+
+    # Intercept flash message to avoid setting a notice
+    # when user is logged in.
+    #
+    def set_flash_message!(key, kind, options = {})
+      super(key, kind, options) unless kind == :signed_in
+    end
   end
 end
