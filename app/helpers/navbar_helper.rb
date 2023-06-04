1da3015e23d809034552fac8ab2567ae62c33ffe
Sebastien Savater <ssavater@mon-territoire.fr>
2023-06-04 12:24:33 +0200

    Add a bit of responsiveness

---
+++ app/helpers/navbar_helper.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module NavbarHelper
+  def navbar_link_to(*args, current: :__undef__, **options, &)
+    path    = args.last
+    current = current_path.start_with?(path) if current == :__undef__
+
+    options[:class] = "navbar__link"
+    options[:class] += " navbar__link--current" if current
+
+    link_to(*args, **options, &)
+  end
+end
