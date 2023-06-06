e7b7df056f74c5c4ba70c2c656d7d9df5313f47a
Sebastien Savater <ssavater@mon-territoire.fr>
2023-06-06 22:20:57 +0200

    Refactor building of reports & packages factories

--- spec/factories/packages.rb
+++ spec/factories/packages.rb
@@ -7,13 +7,13 @@ FactoryBot.define do
     action    { Package::ACTIONS.sample }
 
     sequence(:reference) do |n|
-      month = Time.zone.now.strftime('%Y-%m')
+      month = Time.current.strftime("%Y-%m")
       index = n.to_s.rjust(4, "0")
       "#{month}-#{index}"
     end
 
     trait :transmitted do
-      transmitted_at { Time.zone.now }
+      transmitted_at { Time.current }
     end
 
     trait :approved do
@@ -24,6 +24,45 @@ FactoryBot.define do
     trait :rejected do
       transmitted_at { Time.current }
       rejected_at    { Time.current }
+    end
+
+    trait :with_reports do
+      transient do
+        report_size { 1 }
+        sandbox     { false }
+      end
+
+      reports do
+        # First, build a collection of random communes which are respecting the collectivity territory.
+        # It will save time by reducing the number of factories to create and SQL queries to perform.
+        #
+        communes_size = [report_size, 5].min
+        territory     = collectivity.territory
+
+        case territory
+        when Commune
+          communes    = [territory]
+        when Departement
+          communes    = Array.new(communes_size) { association(:commune, departement: territory) }
+        when EPCI
+          departement = territory.departement || association(:departement)
+          communes    = Array.new(communes_size) { association(:commune, departement: departement, epci: territory) }
+        when Region
+          departement = association(:departement, region: territory)
+          communes    = Array.new(communes_size) { association(:commune, departement: departement) }
+        end
+
+        # Finally, build the list of reports, randomly assigning one the communes.
+        #
+        Array.new(report_size) do
+          association(:report,
+            publisher:    publisher,
+            collectivity: collectivity,
+            sandbox:      sandbox,
+            package:      instance,
+            commune:      communes.sample)
+        end
+      end
     end
   end
 end
--- spec/factories/reports.rb
+++ spec/factories/reports.rb
@@ -19,9 +19,52 @@ FactoryBot.define do
     #     - A collectivity is assigned to the report
     #     - The new package inherits the same collectivity
     #
-    package { association(:package, collectivity: collectivity || build(:collectivity)) }
+    # We also allow to define the status of the package through transient attributes
+    #
+    transient do
+      transmitted_package { false }
+      approved_package    { false }
+      rejected_package    { false }
+      debated_package     { false }
+    end
 
-    commune
+    collectivity do
+      publisher = self.publisher || build(:publisher)
+      association(:collectivity, publisher: publisher)
+    end
+
+    package do
+      collectivity = self.collectivity
+      traits = [].tap do |array|
+        array << :transmitted if transmitted_package
+        array << :approved    if approved_package
+        array << :rejected    if rejected_package
+        array << :debated     if debated_package
+      end
+
+      association(:package, *traits, collectivity: collectivity, publisher: self.publisher)
+    end
+
+    # The commune shoud be on the territory of the package collectivity
+    # It will save time by reducing the number of factories to create and SQL queries to perform.
+    #
+    commune do
+      territory = package.collectivity.territory
+
+      case territory
+      when Commune
+        territory
+      when EPCI
+        departement = territory.departement || association(:departement)
+        association(:commune, epci: territory, departement: departement)
+      when Departement
+        association(:commune, departement: territory)
+      when Region
+        departement = association(:departement, region: territory)
+        association(:commune, departement: departement)
+      end
+    end
+
     action  { Report::ACTIONS.sample }
     subject { Report::SUBJECTS.sample }
 
@@ -30,13 +73,20 @@ FactoryBot.define do
       "#{package.reference}-#{index}"
     end
 
-    after(:build) do |report|
-      report.collectivity         ||= report.package&.collectivity || build(:collectivity)
-      report.package.collectivity ||= report.collectivity
+    trait :low_priority do
+      priority { "low" }
+    end
+
+    trait :medium_priority do
+      priority { "medium" }
     end
 
     trait :high_priority do
       priority { "high" }
+    end
+
+    trait :transmitted do
+      transmitted_package { true }
     end
 
     trait :discarded do
@@ -44,14 +94,17 @@ FactoryBot.define do
     end
 
     trait :approved do
+      transmitted
       approved_at { Time.current }
     end
 
     trait :rejected do
+      transmitted
       rejected_at { Time.current }
     end
 
     trait :debated do
+      transmitted
       debated_at { Time.current }
     end
   end
