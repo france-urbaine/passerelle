30484841d1cc77e542a70d26b9e6fcd222d6dd9c
Sebastien Savater <ssavater@mon-territoire.fr>
2023-08-10 08:14:21 +0200

    Refactor components helpers

--- app/components/application_view_component.rb
+++ app/components/application_view_component.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationViewComponent < ViewComponent::Base
+  include ComponentsHelper
+
+  delegate :current_user, :current_organization,
+    :signed_in?, :allowed_to?,
+    :authorized_link_to, :svg_icon,
+    to: :helpers
+
   class LabelOrContent < self
     def initialize(label = nil)
       @label = label
@@ -9,42 +16,6 @@ class ApplicationViewComponent < ViewComponent::Base
 
     def call
       @label || content
-    end
-  end
-
-  # Devise & ActionPolicy helpers
-  delegate :current_user, :current_organization, :signed_in?, :allowed_to?, to: :helpers
-
-  # Helpers methods
-  delegate :svg_icon, :authorized_link_to, to: :helpers
-
-  # Other component helpers
-  delegate :template_modal_component, :template_content_component, to: :helpers
-
-  COMPONENT_HELPERS = {
-    breadcrumbs_component:        "Breadcrumbs::Component",
-    button_component:             "Button::Component",
-    checkboxes_component:         "Checkboxes::Component",
-    card_component:               "Card::Component",
-    datatable_component:          "Datatable::Component",
-    datatable_skeleton_component: "DatatableSkeleton::Component",
-    dropdown_component:           "Dropdown::Component",
-    hidden_field_component:       "HiddenField::Component",
-    form_block_component:         "FormBlock::Component",
-    modal_component:              "Modal::Component",
-    noscript_component:           "Noscript::Component",
-    notification_component:       "Notification::Component",
-    pagination_component:         "Pagination::Component",
-    pagination_counts_component:  "Pagination::Counts::Component",
-    pagination_options_component: "Pagination::Options::Component",
-    pagination_buttons_component: "Pagination::Buttons::Component",
-    radio_buttons_component:      "RadioButtons::Component",
-    search_component:             "Search::Component"
-  }.freeze
-
-  COMPONENT_HELPERS.each do |name, component|
-    define_method name do |*args, **kwargs, &block|
-      render component.constantize.new(*args, **kwargs), &block
     end
   end
 
