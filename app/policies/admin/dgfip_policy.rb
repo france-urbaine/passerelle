835eab6a29def3ee6be214a8507839705a9cbe22
jbailhache <jbailhache@ft.fr>
2023-08-30 12:56:21 +0200

    DGFIP policy

--- app/policies/admin/dgfip_policy.rb
+++ app/policies/admin/dgfip_policy.rb
@@ -3,10 +3,13 @@
 module Admin
   class DGFIPPolicy < ApplicationPolicy
     alias_rule :index?, :new?, :create?, to: :manage?
-    alias_rule :remove_all?, :destroy_all?, :undiscard_all?, to: :manage?
 
     def manage?
       super_admin?
+    end
+
+    def destroy?
+      false
     end
 
     relation_scope do |relation|
@@ -15,17 +18,6 @@ module Admin
       else
         relation.none
       end
-    end
-
-    relation_scope :destroyable do |relation, exclude_current: true|
-      relation = authorized(relation, with: self.class)
-      relation = relation.where.not(id: organization) if dgfip? && exclude_current
-      relation
-    end
-
-    relation_scope :undiscardable do |relation|
-      relation = authorized(relation, with: self.class)
-      relation.with_discarded.discarded
     end
 
     params_filter do |params|
