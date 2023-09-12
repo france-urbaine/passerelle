4ff01d5e34b0506accd4f4c7ecc3c9ab661f7ddd
Sebastien Savater <ssavater@mon-territoire.fr>
2023-09-12 18:45:56 +0200

    Remove useless methods & files

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -23,10 +23,6 @@ module FormHelper
     end
   end
 
-  def hidden_param_input(key, &)
-    render HiddenField::Component.new(key, params[key]) if params.key?(key)
-  end
-
   def display_errors(record, attribute)
     return unless record.respond_to?(:errors)
 
@@ -34,12 +30,6 @@ module FormHelper
       record.errors.messages_for(attribute).each do |error|
         concat tag.div(error, class: "form-block__errors")
       end
-    end
-  end
-
-  def autocomplete_results
-    tag.div class: "autocomplete__list-wrapper" do
-      tag.ul class: "autocomplete__list", data: { autocomplete_target: "results" }
     end
   end
 end
