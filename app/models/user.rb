3e3ecd303073ff7a52ed01faf731de593490b7f0
Sebastien Savater <s.savater@solutions-territoire.fr>
2023-09-14 15:22:02 +0200

    Inform the user who can no longer receive OTP by email

--- app/models/user.rb
+++ app/models/user.rb
@@ -259,6 +259,10 @@ class User < ApplicationRecord
     otp_method == "email" && organization&.allow_2fa_via_email?
   end
 
+  def otp_no_longer_permitted_by_email?
+    otp_method == "email" && !organization&.allow_2fa_via_email?
+  end
+
   # Devise delivery method
   # ----------------------------------------------------------------------------
   def send_devise_notification(notification, *)
