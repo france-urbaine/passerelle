4f7c43b5fec79b36cf69b0a4aec9bea9d54ea7aa
Sebastien Savater <s.savater@solutions-territoire.fr>
2023-09-18 15:46:30 +0200

    Migrate some components to ERB

--- spec/components/previews/icon/component_preview_spec.rb
+++ spec/components/previews/icon/component_preview_spec.rb
@@ -8,5 +8,4 @@ RSpec.describe Icon::ComponentPreview, type: :component do
   it { is_expected.to render_preview_without_exception(:with_heroicon_outline_set) }
   it { is_expected.to render_preview_without_exception(:with_heroicon_solid_set) }
   it { is_expected.to render_preview_without_exception(:with_title) }
-  it { is_expected.to render_preview_without_exception(:priority_icon) }
 end
