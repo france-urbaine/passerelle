df9d909f0f600178a4bc65621a00a62b70f05401
jbailhache <jbailhache@ft.fr>
2023-10-03 09:57:15 +0200

    WIP reports API doc

--- app/controllers/api/reports_controller.rb
+++ app/controllers/api/reports_controller.rb
@@ -4,6 +4,20 @@ module API
   class ReportsController < ApplicationController
     before_action :authorize!
 
+    resource_description do
+      resource_id "signalements"
+      name  "Signalements"
+      short "Signalements"
+      error 404, "Missing"
+      formats ["json"]
+      deprecated false
+      meta icon: "document-duplicate"
+      description <<-DESC
+        Un signalement fait aux DDFIPs de la part d'une collectivité.
+      DESC
+    end
+
+    api :POST, "/transmissions/:transmission_id/signalements"
     def create
       transmission = find_and_authorize_transmission
 
