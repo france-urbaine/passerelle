d4e39074e1b602451c3c026982160b3adc867123
Jean-Bertrand Couret <38503884+jbcrt@users.noreply.github.com>
2023-10-04 18:50:42 +0200

    Add Reports::UpdateService specs and cleanup Reports::CreateService

--- spec/services/reports/create_service_spec.rb
+++ spec/services/reports/create_service_spec.rb
@@ -18,38 +18,4 @@ RSpec.describe Reports::CreateService do
       .and change(report, :persisted?).from(false).to(true)
       .and not_change(Package, :count)
   end
-
-  # it "creates a package and a report" do
-  #   expect { service.save }
-  #     .to  change(Package, :count).by(1)
-  #     .and change(Report, :count).by(1)
-  #     .and change(report, :persisted?).from(false).to(true)
-  #     .and change(report, :package).to(be_present)
-  # end
-
-  # it "assigns expected attributes" do
-  #   Timecop.travel(Time.zone.local(2023, 5, 26))
-  #   service.save
-
-  #   aggregate_failures do
-  #     expect(report.collectivity).to be(collectivity)
-  #     expect(report.package).to have_attributes(
-  #       form_type: attributes[:form_type],
-  #       reference:  "2023-05-0001"
-  #     )
-  #     expect(report).to have_attributes(
-  #       form_type: attributes[:form_type],
-  #       reference: "2023-05-0001-00001"
-  #     )
-  #   end
-  # end
-
-  # it "increments reference when existing package has already some reports" do
-  #   Timecop.travel(Time.zone.local(2023, 5, 26))
-  #   FactoryBot.rewind_sequences
-  #   create(:package, :evaluation_local_habitation, :with_reports, report_size: 3, collectivity: collectivity, reference: "2023-05-0003")
-
-  #   service.save
-  #   expect(report).to have_attributes(reference: "2023-05-0003-00004")
-  # end
 end
