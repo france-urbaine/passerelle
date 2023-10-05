fe43433862e90b3304b3b8217b4ed9c733eb396d
Glorian Geoffrey <gglorian@fiscalite-territoire.fr>
2023-10-05 16:35:45 +0200

    report api specs

--- spec/requests/api/reports/create_spec.rb
+++ spec/requests/api/reports/create_spec.rb
@@ -4,20 +4,19 @@ require "rails_helper"
 
 RSpec.describe "API::ReportsController#create" do
   subject(:request) do
-    post "/api/transmissions/#{transmission_id}/signalements", as:, headers:, params:
+    post "/api/transmissions/#{transmission.id}/signalements", as:, headers:, params:
   end
 
   let(:as)      { |e| e.metadata.fetch(:as, :json) }
   let(:headers) { |e| e.metadata.fetch(:headers, {}).merge(authorization_header) }
   let(:params)  { |e| e.metadata.fetch(:params, { report: attributes }) }
   let!(:transmission) { create(:transmission, :made_through_api) }
-  let(:transmission_id) { transmission.id }
-  let(:attributes) { attributes_for(:report) }
-  let!(:report_completeness_service) { instance_double(Reports::CheckCompletenessService, valid?: completeness_validity, errors: completeness_errors) }
-  let!(:completeness_validity) { true }
-  let!(:completeness_errors) { nil }
+  let!(:attributes) { attributes_for(:report) }
+  # let!(:report_completeness_service) { instance_double(Reports::CheckCompletenessService, valid?: completeness_validity, errors: completeness_errors) }
+  # let!(:completeness_validity) { true }
+  # let!(:completeness_errors) { nil }
 
-  before { allow(Reports::CheckCompletenessService).to receive(:new).with(instance_of(Report)).and_return(report_completeness_service) }
+  # before { allow(Reports::CheckCompletenessService).to receive(:new).with(instance_of(Report)).and_return(report_completeness_service) }
 
   describe "authorizations" do
     it_behaves_like "it requires an authentication through OAuth in JSON"
@@ -50,29 +49,26 @@ RSpec.describe "API::ReportsController#create" do
 
       it "returns the new report ID" do
         request
-        expect(response).to have_json_body(id: Report.last.id)
+        expect(response.parsed_body).to eq(id: Report.last.id)
       end
     end
 
     context "with invalid report parameters" do
-      let(:report) { build(:report) }
-
       before do
-        allow(Report).to receive(:new).and_return(report)
-        allow(report).to receive(:valid?).and_return(false)
+        attributes[:code_insee] = "invalid"
       end
 
-      it { expect(response).to have_http_status(:success) }
+      it { expect(response).to have_http_status(:unprocessable_entity) }
       it { expect { request }.not_to change(Report, :count) }
 
       it "responds with the report errors" do
-        expect(response).to have_json_body(errors: instance_of(Hash))
+        expect(response.parsed_body).to eq("errors" => { "code_insee" => ["n'est pas valide"] })
       end
     end
 
     context "with invalid report completeness" do
-      let(:completeness_validity) { false }
-      let(:completeness_errors) { { errors: "erreur" } }
+      # let(:completeness_validity) { false }
+      # let(:completeness_errors) { { errors: "erreur" } }
 
       it { expect(response).to have_http_status(:unprocessable_entity) }
       it { expect { request }.not_to change(Report, :count) }
