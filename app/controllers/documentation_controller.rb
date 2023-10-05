367be16cfff52a09c624f1f1d2c2c1ae5bae6d09
Sebastien Savater <s.savater@solutions-territoire.fr>
2023-10-05 15:39:39 +0200

    Protect access to documenation behind authentication

--- app/controllers/documentation_controller.rb
+++ app/controllers/documentation_controller.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
 class DocumentationController < ApplicationController
-  respond_to :html
-
   before_action :validate_documentation_path
-
-  skip_before_action :authenticate_user!
   skip_after_action :verify_authorized
 
   prepend_view_path "app/views/documentation/api"
