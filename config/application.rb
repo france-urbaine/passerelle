a66550398b035c34c65c0cbbede7739db587ac0d
Sebastien Savater <s.savater@solutions-territoire.fr>
2023-10-05 15:38:07 +0200

    Configure multiple domain in application

--- config/application.rb
+++ config/application.rb
@@ -43,6 +43,46 @@ module Fiscahub
     # Use a real queuing backend for Active Job
     config.active_job.queue_adapter = :sidekiq
 
+    # Define domain strategy.
+    # Default domain is `localhost`
+    #
+    # To be able to use subdomains (such as api.), you need a domain with a longer TLD.
+    # For example, use:
+    #
+    #   DOMAIN_APP = localhost.local
+    #
+    # You'll then be able to run api.localhost.local
+    #
+    unless Rails.env.test?
+      config.x.domain = ENV.fetch("DOMAIN_APP", "localhost")
+      config.hosts << ".#{config.x.domain}"
+    end
+
+    # If your main domain is already a subdomain (such as alpha.fiscahub.fr),
+    # you should also define TLD length:
+    #
+    #   DOMAIN_APP = alpha.fiscahub.fr
+    #   DOMAIN_TLD_LENGTH = 2
+    #
+    config.action_dispatch.tld_length = Integer(ENV["DOMAIN_TLD_LENGTH"] || 1)
+
+    # Session are linked to default domain (DOMAIN_APP)
+    # To share cookies between two main domains (fiscahub.fr & alpha.fiscahub.fr),
+    # you may define the same DOMAIN_COOKIE:
+    #
+    # - production:
+    #   DOMAIN_APP = fiscahub.fr
+    #
+    # - staging:
+    #   DOMAIN_APP = alpha.fiscahub.fr
+    #   DOMAIN_COOKIE = fiscahub.fr
+    #
+    config.session_store :cookie_store,
+      key:        "_fiscahub_session",
+      domain:     ENV.fetch("DOMAIN_COOKIE", :all),
+      tld_length: 2
+
+    # Application-wide constants
     DEFAULT_COMMUNES_URL = "https://www.insee.fr/fr/statistiques/fichier/2028028/table-appartenance-geo-communes-23.zip"
     DEFAULT_EPCIS_URL    = "https://www.insee.fr/fr/statistiques/fichier/2510634/Intercommunalite_Metropole_au_01-01-2023.zip"
   end
