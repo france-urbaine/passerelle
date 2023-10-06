486005ee9d1435665fac3a8f67508ea6c5a1efc1
Sebastien Savater <s.savater@solutions-territoire.fr>
2023-10-06 16:37:11 +0200

    Fix migration

--- db/migrate/20230926152855_update_doorkeeper_owner_type.rb
+++ db/migrate/20230926152855_update_doorkeeper_owner_type.rb
@@ -2,6 +2,9 @@
 
 class UpdateDoorkeeperOwnerType < ActiveRecord::Migration[7.0]
   def change
+    remove_foreign_key :oauth_access_grants, :users, column: :resource_owner_id
+    remove_foreign_key :oauth_access_tokens, :users, column: :resource_owner_id
+
     add_foreign_key :oauth_access_grants, :publishers, column: :resource_owner_id, on_delete: :cascade
     add_foreign_key :oauth_access_tokens, :publishers, column: :resource_owner_id, on_delete: :cascade
   end
