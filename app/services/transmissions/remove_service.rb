35f16892313c5e825ec7eced4b4196993b813302
Jean-Bertrand Couret <38503884+jbcrt@users.noreply.github.com>
2023-10-13 12:05:16 +0200

    Refacto Transmissions::RemoveService with new ReportStates scope

--- app/services/transmissions/remove_service.rb
+++ app/services/transmissions/remove_service.rb
@@ -12,12 +12,12 @@ module Transmissions
     end
 
     def remove(reports)
-      @before_transmission_reports_count = @transmission.reports.count
+      @before_transmission_reports_count = Report.in_transmission(@transmission.id).count
       removable_reports                  = @transmission.reports.where(id: reports.ids)
 
       removable_reports.update_all(transmission_id: nil)
 
-      @after_transmission_reports_count = @transmission.reports.count
+      @after_transmission_reports_count = Report.in_transmission(@transmission.id).count
       @removed_reports_count            = @before_transmission_reports_count - @after_transmission_reports_count
 
       self
