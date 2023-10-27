cbebf1e18031efce76eef5a5a9d416b9d0f42110
Sebastien Savater <s.savater@solutions-territoire.fr>
2023-10-27 17:31:01 +0200

    Fix inspect_html for system specs

--- spec/support/inspect_html.rb
+++ spec/support/inspect_html.rb
@@ -4,7 +4,14 @@ require "htmlbeautifier"
 
 module InspectHTMLCapybara
   def inspect_html(node = page)
-    puts HtmlBeautifier.beautify(node.native.inner_html)
+    html =
+      case node
+      when Capybara::Session      then node.html
+      when Capybara::Node::Simple then node.native.inner_html
+      when Capybara::Node::Base   then node.native["innerHTML"]
+      end
+
+    puts HtmlBeautifier.beautify(html)
   end
 end
 
