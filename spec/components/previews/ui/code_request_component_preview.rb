ec6c7893df2d85aab5f5a867c0c9b58022eef547
Glorian Geoffrey <gglorian@fiscalite-territoire.fr>
2023-11-06 16:56:28 +0100

    code_request_preview

---
+++ spec/components/previews/ui/code_request_component_preview.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module UI
+  # @display frame "content"
+  #
+  class CodeRequestComponentPreview < ViewComponent::Preview
+    # @label Default
+    #
+    def default; end
+
+    # @label With header
+    #
+    def with_header; end
+
+    # @label With body
+    #
+    def with_body; end
+
+    # @label With File
+    #
+    def with_file; end
+  end
+end
