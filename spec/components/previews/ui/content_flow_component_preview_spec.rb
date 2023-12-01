0b4e549a419b06872fba2d7ab1609ccdffd0e1ab
Sebastien Savater <s.savater@solutions-territoire.fr>
2023-12-01 15:06:12 +0100

    Remove ContentFlowComponent (replaced by ContentLayoutComponent)

--- spec/components/previews/ui/content_flow_component_preview_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require "rails_helper"
-
-RSpec.describe UI::ContentFlowComponentPreview, type: :component do
-  it { is_expected.to render_preview_without_exception(:default) }
-  it { is_expected.to render_preview_without_exception(:with_actions) }
-  it { is_expected.to render_preview_without_exception(:without_header) }
-end
