c3a71495055b7390ea9a4c6827500a58c76f9e33
Jérôme LAMARQUE <jerome.lamarque@gmail.com>
2023-12-11 10:36:36 +0100

    Audits : additional tests (routing)

---
+++ spec/routing/organization/oauth_applications/audits_routing_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Organization::OauthApplications::AuditsController do
+  it { expect(get:    "/organisation/oauth_applications/9c6c00c4/audits").to route_to("organization/oauth_applications/audits#index", oauth_application_id: "9c6c00c4") }
+  it { expect(post:   "/organisation/oauth_applications/9c6c00c4/audits").to be_unroutable }
+  it { expect(patch:  "/organisation/oauth_applications/9c6c00c4/audits").to be_unroutable }
+  it { expect(delete: "/organisation/oauth_applications/9c6c00c4/audits").to be_unroutable }
+
+  it { expect(get:    "/organisation/oauth_applications/9c6c00c4/audits/new").to       be_unroutable }
+  it { expect(get:    "/organisation/oauth_applications/9c6c00c4/audits/edit").to      be_unroutable }
+  it { expect(get:    "/organisation/oauth_applications/9c6c00c4/audits/remove").to    be_unroutable }
+  it { expect(get:    "/organisation/oauth_applications/9c6c00c4/audits/undiscard").to be_unroutable }
+  it { expect(patch:  "/organisation/oauth_applications/9c6c00c4/audits/undiscard").to be_unroutable }
+
+  it { expect(get:    "/organisation/oauth_applications/9c6c00c4/audits/b12170f4").to be_unroutable }
+  it { expect(post:   "/organisation/oauth_applications/9c6c00c4/audits/b12170f4").to be_unroutable }
+  it { expect(patch:  "/organisation/oauth_applications/9c6c00c4/audits/b12170f4").to be_unroutable }
+  it { expect(delete: "/organisation/oauth_applications/9c6c00c4/audits/b12170f4").to be_unroutable }
+
+  it { expect(get:    "/organisation/oauth_applications/9c6c00c4/audits/b12170f4/edit").to      be_unroutable }
+  it { expect(get:    "/organisation/oauth_applications/9c6c00c4/audits/b12170f4/remove").to    be_unroutable }
+  it { expect(get:    "/organisation/oauth_applications/9c6c00c4/audits/b12170f4/undiscard").to be_unroutable }
+  it { expect(patch:  "/organisation/oauth_applications/9c6c00c4/audits/b12170f4/undiscard").to be_unroutable }
+end
