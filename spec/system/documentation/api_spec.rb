69c28a9034103821e907670b0078d169f4363f76
Sebastien Savater <s.savater@solutions-territoire.fr>
2023-12-28 09:12:19 +0100

    Add system specs to test API guides

--- spec/system/documentation/api_spec.rb
+++ spec/system/documentation/api_spec.rb
@@ -37,13 +37,63 @@ RSpec.describe "API Documentation" do
     expect(page).to have_selector("h1", text: "La page que vous recherchez n'est pas disponible.")
   end
 
-  it "visits A Propos" do
+  it "visits « A Propos » page" do
     visit api_documentation_url("guides/a_propos")
     expect(page).to have_selector("h1", text: "A propos")
   end
 
-  it "visits Authentification" do
+  it "visits « Authentification » page" do
     visit api_documentation_url("guides/authentification")
     expect(page).to have_selector("h1", text: "Authentification")
+  end
+
+  it "visits « Sandbox » page" do
+    visit api_documentation_url("guides/sandbox")
+    expect(page).to have_selector("h1", text: "Mode « Bac à sable »")
+  end
+
+  it "visits « Transmettre des signalements » page" do
+    visit api_documentation_url("guides/transmettre_des_signalements")
+    expect(page).to have_selector("h1", text: "Transmettre des signalements")
+  end
+
+  it "visits « Transmettre des pièces jointes » page" do
+    visit api_documentation_url("guides/transmettre_des_pieces_jointes")
+    expect(page).to have_selector("h1", text: "Transmettre des pièces jointes")
+  end
+
+  it "visits « Formulaires de signalement » page" do
+    visit api_documentation_url("guides/formulaires")
+
+    expect(page).to have_selector("h1", text: "Formulaires de signalement")
+
+    click_on "Evaluation d'un local d'habitation"
+
+    expect(page).to have_selector("h1", text: "Evaluation d'un local d'habitation")
+
+    go_back
+    click_on "Evaluation d'un local professionnel"
+
+    expect(page).to have_selector("h1", text: "Evaluation d'un local professionnel")
+
+    go_back
+    click_on "Création d'un local professionnel"
+
+    expect(page).to have_selector("h1", text: "Création d'un local professionnel")
+
+    go_back
+    click_on "Création d'un local professionnel"
+
+    expect(page).to have_selector("h1", text: "Création d'un local professionnel")
+
+    go_back
+    click_on "Occupation d'un local professionnel"
+
+    expect(page).to have_selector("h1", text: "Occupation d'un local professionnel")
+
+    go_back
+    click_on "Occupation d'un local professionnel"
+
+    expect(page).to have_selector("h1", text: "Occupation d'un local professionnel")
   end
 end
