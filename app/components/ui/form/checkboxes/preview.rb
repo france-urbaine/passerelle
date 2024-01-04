25414f4000b80b552cb42f0d1bb1dc1c509b2cc2
Glorian Geoffrey <gglorian@fiscalite-territoire.fr>
2024-01-04 17:45:37 +0100

    checkboxes component

---
+++ app/components/ui/form/checkboxes/preview.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module UI
+  module Form
+    module Checkboxes
+      # @display frame "content"
+      #
+      class Preview < ApplicationViewComponentPreview
+        # @label Default
+        #
+        def default; end
+
+        # @label With a form builder
+        #
+        def with_formbuilder; end
+      end
+    end
+  end
+end
--- spec/components/ui/form/checkboxes/preview_spec.rb
+++ spec/components/ui/form/checkboxes/preview_spec.rb
@@ -2,7 +2,7 @@
 
 require "rails_helper"
 
-RSpec.describe UI::Form::CheckboxesComponentPreview do
+RSpec.describe UI::Form::Checkboxes::Preview do
   it { is_expected.to render_preview_without_exception(:default) }
   it { is_expected.to render_preview_without_exception(:with_formbuilder) }
 end
