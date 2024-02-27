4463626174a9e58185bf4d1d59fa173b9554fac1
jbailhache <jbailhache@ft.fr>
2024-02-27 10:47:12 +0100

    Set resolution_motif when migrating

--- db/migrate/20240221092903_add_resolution_motif_to_report.rb
+++ db/migrate/20240221092903_add_resolution_motif_to_report.rb
@@ -7,5 +7,12 @@ class AddResolutionMotifToReport < ActiveRecord::Migration[7.1]
     change_table :reports, bulk: true do |t|
       t.column :resolution_motif, :enum, enum_type: :resolution_motif
     end
+
+    up_only do
+      execute <<-SQL.squish
+        UPDATE reports SET resolution_motif = 'maj_local' WHERE state IN ('applicable', 'approved');
+        UPDATE reports SET resolution_motif = 'absence_incoherence' WHERE state IN ('inapplicable', 'canceled');
+      SQL
+    end
   end
 end
