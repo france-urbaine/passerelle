e607538d9251181c914e755e4318e831dddace14
Sebastien Savater <s.savater@solutions-territoire.fr>
2024-02-29 12:25:37 +0100

    Fix preview of Reports::StatusBadge

--- app/components/views/reports/status_badge/preview.rb
+++ app/components/views/reports/status_badge/preview.rb
@@ -4,7 +4,7 @@ module Views
   module Reports
     module StatusBadge
       # @display frame "content"
-      # @display width "medium"
+      # @display width "w-[60rem]"
       #
       class Preview < ApplicationViewComponentPreview
         def default; end
