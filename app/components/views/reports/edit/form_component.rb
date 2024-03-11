02a17a1c1016962e69f83f705892ac2e0df18763
Sebastien Savater <s.savater@solutions-territoire.fr>
2024-03-11 09:48:38 +0100

    Add notes about what is shared to collectivities, refactor some CSS

--- app/components/views/reports/edit/form_component.rb
+++ app/components/views/reports/edit/form_component.rb
@@ -6,7 +6,7 @@ module Views
       class FormComponent < ApplicationViewComponent
         Enjeu              = Class.new(self)
         Observations       = Class.new(self)
-        Reponse            = Class.new(self)
+        Response           = Class.new(self)
         Note               = Class.new(self)
         PropositionAdresse = Class.new(self)
 
