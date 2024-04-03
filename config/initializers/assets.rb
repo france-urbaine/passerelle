f9856c632a131fd00b8bd2c9fdc3a0b84875b8bb
Sebastien Savater <github.60k5k@simplelogin.co>
2024-04-03 16:27:52 +0200

    Avoid precompiling of usesless assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,9 +13,23 @@ Rails.application.config.assets.version = "1.0"
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
-# Remove actiontext from precompiled assets
 Rails.application.config.after_initialize do
+  # Remove turbo & stimulus from precompiled assets:
+  # they are compiled in application.js by esbuild
+  #
+  Rails.application.config.assets.precompile.delete("turbo.js")
+  Rails.application.config.assets.precompile.delete("turbo.min.js")
+  Rails.application.config.assets.precompile.delete("turbo.min.js.map")
+
+  Rails.application.config.assets.precompile.delete("stimulus.js")
+  Rails.application.config.assets.precompile.delete("stimulus.min.js")
+  Rails.application.config.assets.precompile.delete("stimulus.min.js.map")
+
+  # Remove actiontext:
+  # we dont use it
+  #
   Rails.application.config.assets.precompile.delete("actiontext.js")
+  Rails.application.config.assets.precompile.delete("actiontext.esm.js")
   Rails.application.config.assets.precompile.delete("trix.js")
   Rails.application.config.assets.precompile.delete("trix.css")
 end
