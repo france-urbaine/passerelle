b121f0fe294449b99498881babea5b3df9eb4e93
Sebastien Savater <github.60k5k@simplelogin.co>
2024-05-03 17:20:50 +0200

    Exclude previews from tailwind compilation in production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,28 @@
-module.exports = {
-  content: [
-    './app/views/**/*.html.erb',
-    './app/views/**/*.html.slim',
-    './app/views/**/*.html+*.erb',
-    './app/views/**/*.html+*.slim',
-    './app/helpers/**/*.rb',
-    './app/components/**/*.html.erb',
-    './app/components/**/*.html.slim',
-    './app/components/**/*.rb',
-    './app/javascript/**/*.js',
+let content = [
+  './app/views/**/*.html.erb',
+  './app/views/**/*.html.slim',
+  './app/views/**/*.html+*.erb',
+  './app/views/**/*.html+*.slim',
+  './app/helpers/**/*.rb',
+  './app/components/**/*.html.erb',
+  './app/components/**/*.html.slim',
+  './app/components/**/*.rb',
+  './app/javascript/**/*.js'
+]
+
+if (process.env.RAILS_ENV == "production") {
+  content.push(
+    '!./app/components/**/previews/*.html.erb',
+    '!./app/components/**/previews/*.html.slim'
+  )
+} else {
+  content.push(
     './spec/components/previews/**/*.rb',
     './spec/components/previews/**/*.html.slim',
     './spec/components/previews/**/*.html.erb'
-  ]
+  )
+}
+
+module.exports = {
+  content: content
 }
