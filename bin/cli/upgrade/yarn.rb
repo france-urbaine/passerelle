0f705e839ed8dd9cac7e83c9ac835052789222de
Sebastien Savater <github.60k5k@simplelogin.co>
2024-09-07 11:56:36 +0200

    Update bin/upgrade yarn to rebuild assets & run system specs

--- bin/cli/upgrade/yarn.rb
+++ bin/cli/upgrade/yarn.rb
@@ -9,23 +9,38 @@ module CLI
         say "Updating JS dependencies"
         run "yarn upgrade-interactive --latest"
 
-        if package_changed? && yarn_lock_changed?
+        if yarn_lock_changed?
           say ""
-          say "package.json & yarn.lock have changed."
-          say ""
-          say "Would you like to see changes on package.json ? [Yn]"
-          run "git diff package.json" if ask == "Y"
+
+          if package_changed?
+            say "package.json & yarn.lock have changed."
+          else
+            say "yarn.lock has changed."
+          end
 
           say ""
-          say "Would you like to commit changes on yarn.lock & package.json ? [Yn]"
-          run "git commit -m \"Update JS dependencies\" -- package.json yarn.lock" if ask == "Y"
+          say "Would you like to run the system specs ? [Yn]"
 
-        elsif yarn_lock_changed?
+          if ask == "Y"
+            say "Updating assets"
+            run "bin/setup test assets"
+            run "bin/ci test system"
+          end
+
           say ""
-          say "yarn.lock has changed."
-          say ""
-          say "Would you like to commit changes on yarn.lock ? [Yn]"
-          run "git commit -m \"Update JS dependencies\" -- yarn.lock" if ask == "Y"
+
+          if package_changed?
+            say "Would you like to see changes on package.json ? [Yn]"
+            run "git diff package.json" if ask == "Y"
+
+            say ""
+            say "Would you like to commit changes on yarn.lock & package.json ? [Yn]"
+            run "git commit -m \"Update JS dependencies\" -- package.json yarn.lock" if ask == "Y"
+
+          else
+            say "Would you like to commit changes on yarn.lock ? [Yn]"
+            run "git commit -m \"Update JS dependencies\" -- yarn.lock" if ask == "Y"
+          end
         end
 
         say ""
