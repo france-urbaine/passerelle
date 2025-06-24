f63fd9260f0da45d54ae3a3792ef730296dae4df
jeanne <5803441+jeanne-b-@users.noreply.github.com>
2025-06-24 16:12:43 +0200

    Rework switch stimulus controller to handle more use cases

--- app/javascript/controllers/switch_controller.js
+++ app/javascript/controllers/switch_controller.js
@@ -1,24 +1,77 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["target"]
+  static targets = ["showWhenEmpty", "hide", "show"]
 
-  toggle (event) {
-    const value = event.target.value + ""
+  toggle(event) {
+    const group = event.params.group
+    const updatedValues = this.inputValues(event.target)
+    const empty = !updatedValues.length || (updatedValues.length == 1 && updatedValues[0] === "")
 
-    this.targetTargets.forEach((target) => {
-      let targetValue = target.dataset.switchValue + ""
-      let enabled = (value === targetValue)
-
-      const separator = target.dataset.switchValueSeparator
-
-      if (separator && separator.length > 0) {
-        targetValue = targetValue.split(separator)
-        enabled = targetValue.includes(value)
+    this.showWhenEmptyTargets.forEach(target => {
+      if (this.targetMatchGroup(target, group)) {
+        target.hidden   = !empty
+        target.disabled = !empty
       }
-
-      target.hidden = !enabled
-      target.disabled = !enabled
     })
+
+    this.showTargets.forEach(target => {
+      if (this.targetMatchGroup(target, group)) {
+        const match = this.targetMatchValues(target, updatedValues)
+
+        target.hidden   = !match
+        target.disabled = !match
+      }
+    })
+
+    this.hideTargets.forEach(target => {
+      if (this.targetMatchGroup(target, group)) {
+        const match = this.targetMatchValues(target, updatedValues)
+
+        target.hidden   = match
+        target.disabled = match
+      }
+    })
+  }
+
+  inputValues(target) {
+    if (target.type == "checkbox") {
+      return Array.from(document.querySelectorAll("input[name='" + target.name + "']:not(:disabled)"))
+        .filter(input => input.checked)
+        .map(input => String(input.value))
+
+    } else if (target.type == "select-multiple") {
+      return Array.from(target.selectedOptions)
+        .map(option => String(option.value))
+
+    } else {
+      return [String(target.value)]
+    }
+  }
+
+  targetMatchGroup(target, group) {
+    return !group || group === target.dataset.switchTargetGroup
+  }
+
+  targetMatchValues(target, updatedValues) {
+    const targetValues = this.getTargetValues(target)
+
+    return targetValues.some(value => updatedValues.includes(value))
+  }
+
+  getTargetValues(target) {
+    const value = target.dataset.switchValue
+    const values = target.dataset.switchValues
+    const separator = target.dataset.switchValueSeparator
+
+    if (separator && separator.length > 0 && value !== undefined) {
+      return value.split(separator)
+    } else if (value !== undefined) {
+      return [value + ""]
+    } else if (values !== undefined) {
+      return JSON.parse(values).map(String)
+    } else {
+      return []
+    }
   }
 }
