990b52f88c0df0d69e5acec9f92759d20320dc84
jeanne <5803441+jeanne-b-@users.noreply.github.com>
2025-07-10 12:25:01 +0200

    Validate that a supervisor cannot create a user without offices to avoid creating users they can't see

--- app/services/users/create_service.rb
+++ app/services/users/create_service.rb
@@ -7,12 +7,16 @@ module Users
     #   def organization_name=
     #   def office_ids=
 
+    delegate :office_users, to: :record
+
     def initialize(*, invited_by: nil, organization: nil)
       super(*)
 
       @inviter      = invited_by
       @organization = organization
     end
+
+    validate :any_office_users, if: :inviter_is_a_supervisor?
 
     before_validation do
       user.password = Devise.friendly_token[0, 20]
@@ -22,6 +26,18 @@ module Users
     before_save do
       user.inviter = @inviter
       user.invited_at = Time.current
+    end
+
+    private
+
+    def any_office_users
+      errors.add(:office_users, :blank) if office_users.empty?
+      record.errors.add(:office_users, :blank) if office_users.empty?
+    end
+
+    def inviter_is_a_supervisor?
+      !@inviter.super_admin && !@inviter.organization_admin &&
+        @inviter.office_users.where(supervisor: true).any?
     end
   end
 end
