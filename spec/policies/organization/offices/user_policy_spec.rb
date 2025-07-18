f2b5909a066c34d5fb7242eff909cc8e9584915a
jeanne <5803441+jeanne-b-@users.noreply.github.com>
2025-07-18 15:44:21 +0200

    Update organization offices users controller and policy so it checks for office in context

--- spec/policies/organization/offices/user_policy_spec.rb
+++ spec/policies/organization/offices/user_policy_spec.rb
@@ -3,21 +3,59 @@
 require "rails_helper"
 
 RSpec.describe Organization::Offices::UserPolicy, type: :policy do
+  let(:office)  { build_stubbed(:office) }
+  let(:context) { { user: current_user, office: office } }
+
+  shared_context "when using the current user's office" do
+    let(:office) do
+      (current_user.office_users.any? && current_user.office_users.first.office) ||
+        build(:office)
+    end
+  end
+
+  shared_context "when the record is in the office", stub_factories: false do
+    let(:record) { create(:user, :with_office, office:, organization: current_organization) }
+  end
+
   describe_rule :manage? do
     context "without record" do
       let(:record) { User }
 
       it_behaves_like("when current user is a super admin")        { failed }
       it_behaves_like("when current user is a DDFIP admin")        { succeed }
-      it_behaves_like("when current user is a DDFIP supervisor")   { succeed }
+      it_behaves_like("when current user is a DDFIP supervisor")   { failed }
       it_behaves_like("when current user is a DDFIP user")         { failed }
       it_behaves_like("when current user is a publisher admin")    { failed }
       it_behaves_like("when current user is a publisher user")     { failed }
       it_behaves_like("when current user is a collectivity admin") { failed }
       it_behaves_like("when current user is a collectivity user")  { failed }
+
+      context "without office in context" do
+        let(:context) { super().without(:office) }
+
+        it_behaves_like("when current user is a super admin")        { failed }
+        it_behaves_like("when current user is a DDFIP admin")        { succeed }
+        it_behaves_like("when current user is a DDFIP supervisor")   { succeed }
+        it_behaves_like("when current user is a DDFIP user")         { failed }
+        it_behaves_like("when current user is a publisher admin")    { failed }
+        it_behaves_like("when current user is a publisher user")     { failed }
+        it_behaves_like("when current user is a collectivity admin") { failed }
+        it_behaves_like("when current user is a collectivity user")  { failed }
+      end
+
+      it_behaves_like "when using the current user's office" do
+        it_behaves_like("when current user is a super admin")        { failed }
+        it_behaves_like("when current user is a DDFIP admin")        { succeed }
+        it_behaves_like("when current user is a DDFIP supervisor")   { succeed }
+        it_behaves_like("when current user is a DDFIP user")         { failed }
+        it_behaves_like("when current user is a publisher admin")    { failed }
+        it_behaves_like("when current user is a publisher user")     { failed }
+        it_behaves_like("when current user is a collectivity admin") { failed }
+        it_behaves_like("when current user is a collectivity user")  { failed }
+      end
     end
 
-    context "with a collectivity" do
+    context "with an user" do
       let(:record) { build_stubbed(:user) }
 
       it_behaves_like("when current user is a super admin")        { failed }
@@ -28,6 +66,30 @@ RSpec.describe Organization::Offices::UserPolicy, type: :policy do
       it_behaves_like("when current user is a publisher user")     { failed }
       it_behaves_like("when current user is a collectivity admin") { failed }
       it_behaves_like("when current user is a collectivity user")  { failed }
+
+      context "without office in context" do
+        let(:context) { super().without(:office) }
+
+        it_behaves_like("when current user is a super admin")        { failed }
+        it_behaves_like("when current user is a DDFIP admin")        { failed }
+        it_behaves_like("when current user is a DDFIP supervisor")   { failed }
+        it_behaves_like("when current user is a DDFIP user")         { failed }
+        it_behaves_like("when current user is a publisher admin")    { failed }
+        it_behaves_like("when current user is a publisher user")     { failed }
+        it_behaves_like("when current user is a collectivity admin") { failed }
+        it_behaves_like("when current user is a collectivity user")  { failed }
+      end
+
+      it_behaves_like "when using the current user's office" do
+        it_behaves_like("when current user is a super admin")        { failed }
+        it_behaves_like("when current user is a DDFIP admin")        { failed }
+        it_behaves_like("when current user is a DDFIP supervisor")   { failed }
+        it_behaves_like("when current user is a DDFIP user")         { failed }
+        it_behaves_like("when current user is a publisher admin")    { failed }
+        it_behaves_like("when current user is a publisher user")     { failed }
+        it_behaves_like("when current user is a collectivity admin") { failed }
+        it_behaves_like("when current user is a collectivity user")  { failed }
+      end
     end
 
     context "with a member of the current organization" do
@@ -35,12 +97,63 @@ RSpec.describe Organization::Offices::UserPolicy, type: :policy do
 
       it_behaves_like("when current user is a super admin")        { failed }
       it_behaves_like("when current user is a DDFIP admin")        { succeed }
-      it_behaves_like("when current user is a DDFIP supervisor")   { succeed }
+      it_behaves_like("when current user is a DDFIP supervisor")   { failed }
       it_behaves_like("when current user is a DDFIP user")         { failed }
       it_behaves_like("when current user is a publisher admin")    { failed }
       it_behaves_like("when current user is a publisher user")     { failed }
       it_behaves_like("when current user is a collectivity admin") { failed }
       it_behaves_like("when current user is a collectivity user")  { failed }
+
+      it_behaves_like "when using the current user's office" do
+        it_behaves_like "when the record is in the office" do
+          it_behaves_like("when current user is a DDFIP admin")      { succeed }
+          it_behaves_like("when current user is a DDFIP supervisor") { succeed }
+          it_behaves_like("when current user is a DDFIP user")       { failed }
+        end
+      end
+
+      context "without office in context" do
+        let(:context) { super().without(:office) }
+
+        it_behaves_like("when current user is a super admin")        { failed }
+        it_behaves_like("when current user is a DDFIP admin")        { succeed }
+        it_behaves_like("when current user is a DDFIP supervisor")   { failed }
+        it_behaves_like("when current user is a DDFIP user")         { failed }
+        it_behaves_like("when current user is a publisher admin")    { failed }
+        it_behaves_like("when current user is a publisher user")     { failed }
+        it_behaves_like("when current user is a collectivity admin") { failed }
+        it_behaves_like("when current user is a collectivity user")  { failed }
+
+        it_behaves_like "when using the current user's office" do
+          it_behaves_like "when the record is in the office" do
+            it_behaves_like("when current user is a super admin")        { failed }
+            it_behaves_like("when current user is a DDFIP admin")        { succeed }
+            it_behaves_like("when current user is a DDFIP supervisor")   { succeed }
+          end
+        end
+      end
+
+      it_behaves_like "when using the current user's office" do
+        it_behaves_like("when current user is a super admin")        { failed }
+        it_behaves_like("when current user is a DDFIP admin")        { succeed }
+        it_behaves_like("when current user is a DDFIP supervisor")   { failed }
+        it_behaves_like("when current user is a DDFIP user")         { failed }
+        it_behaves_like("when current user is a publisher admin")    { failed }
+        it_behaves_like("when current user is a publisher user")     { failed }
+        it_behaves_like("when current user is a collectivity admin") { failed }
+        it_behaves_like("when current user is a collectivity user")  { failed }
+
+        it_behaves_like "when the record is in the office" do
+          it_behaves_like("when current user is a super admin")        { failed }
+          it_behaves_like("when current user is a DDFIP admin")        { succeed }
+          it_behaves_like("when current user is a DDFIP supervisor")   { succeed }
+          it_behaves_like("when current user is a DDFIP user")         { failed }
+          it_behaves_like("when current user is a publisher admin")    { failed }
+          it_behaves_like("when current user is a publisher user")     { failed }
+          it_behaves_like("when current user is a collectivity admin") { failed }
+          it_behaves_like("when current user is a collectivity user")  { failed }
+        end
+      end
     end
   end
 
