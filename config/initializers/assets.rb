ae37093e839c999ee04132c2616583c75deab354
Sebastien Savater <github.60k5k@simplelogin.co>
2025-08-11 12:21:12 +0200

    Upgrade to Rails 8

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,9 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = "1.0"
 
+# Suppress logger output for asset requests.
+Rails.application.config.assets.quiet = true
+
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 
