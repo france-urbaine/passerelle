65bc387831ee490c292c08829db422910a345027
jeanne <5803441+jeanne-b-@users.noreply.github.com>
2025-10-27 16:31:40 +0100

    Make only one request when couting reports for form admin dashboard

--- app/components/views/dashboard/component/ddfip_form_admin.rb
+++ app/components/views/dashboard/component/ddfip_form_admin.rb
@@ -26,25 +26,36 @@ module Views
         end
 
         def reports_transmitted_count
-          @reports.transmitted.count
+          reports_counts.transmitted_count
         end
 
         def reports_unassigned_count
-          @reports.waiting_for_assignment.merge(
-            @reports.waiting_for_acceptance
-          ).count
+          reports_counts.unassigned_count
         end
 
         def reports_approved_count
-          @reports.approved.count
+          reports_counts.approved_count
         end
 
         def reports_canceled_count
-          @reports.canceled.count
+          reports_counts.canceled_count
         end
 
         def reports_rejected_count
-          @reports.rejected.count
+          reports_counts.rejected_count
+        end
+
+        def reports_counts
+          @reports_counts ||=
+            @reports.select(
+              Arel.star.count.filter(Report.arel_table[:state].in(Report::TRANSMITTED_STATES)).as("transmitted_count").to_sql,
+              Arel.star.count.filter(
+                Report.arel_table[:state].in(%w[transmitted acknowledged accepted])
+              ).as("unassigned_count").to_sql,
+              Arel.star.count.filter(Report.arel_table[:state].eq("approved")).as("approved_count").to_sql,
+              Arel.star.count.filter(Report.arel_table[:state].eq("canceled")).as("canceled_count").to_sql,
+              Arel.star.count.filter(Report.arel_table[:state].eq("rejected")).as("rejected_count").to_sql
+            ).reorder(nil)[0]
         end
       end
     end
