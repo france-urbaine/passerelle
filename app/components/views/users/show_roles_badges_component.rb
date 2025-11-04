25436ee0d177a4fdb8fde4c303820b3d87c82837
jeanne <5803441+jeanne-b-@users.noreply.github.com>
2025-11-04 11:25:44 +0100

    Add form admin to user roles in users lists

--- app/components/views/users/show_roles_badges_component.rb
+++ app/components/views/users/show_roles_badges_component.rb
@@ -12,11 +12,18 @@ module Views
         @user.organization_type == "DDFIP" &&
           !@user.super_admin? &&
           !@user.organization_admin? &&
+          !form_admin? &&
           @user.offices.empty?
       end
 
       def supervised_office_ids
         @user.office_users.filter_map { it.office_id if it.supervisor? }
+      end
+
+      def form_admin?
+        @user.organization_type == "DDFIP" &&
+          !@user.organization_admin? &&
+          @user.user_form_types.any?
       end
     end
   end
