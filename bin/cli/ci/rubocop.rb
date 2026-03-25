557a60f8510000886ea53e8fae856914b6c6669d
Sebastien Savater <github.60k5k@simplelogin.co>
2026-03-25 15:44:28 +0100

    Upgrade rubocop in CI

--- bin/cli/ci/rubocop.rb
+++ bin/cli/ci/rubocop.rb
@@ -8,11 +8,15 @@ module CLI
       def call(*args)
         say "Analyzing code issues with Rubocop"
 
-        command = "bundle exec rubocop"
-        command += " --server --display-cop-names" unless ENV["CI"] == "true"
-        command += " #{args.join(' ')}" if args.any?
+        command = %w[bundle exec rubocop]
 
-        run command
+        # explicit rubocop config increases performance slightly while avoiding config confusion.
+        command << " --config .rubocop.yml"
+        command << " --server --display-cop-names" unless ENV["CI"] == "true"
+
+        command += args
+
+        run command.join(" ")
       end
     end
   end
