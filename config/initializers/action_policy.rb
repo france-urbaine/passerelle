4437063cb6498abd36107b27858ec662258626d5
Sebastien Savater <github.60k5k@simplelogin.co>
2026-03-26 19:47:12 +0100

    Update ActionPolicy caching & instrumentation

--- config/initializers/action_policy.rb
+++ config/initializers/action_policy.rb
@@ -3,30 +3,40 @@
 # See https://actionpolicy.evilmartians.io to configure ActionPolicy
 # Example:
 #
-# Rails.application.configure do
-#   config.action_policy.cache_store = Rails.cache
-# end
-
-ActiveSupport::Notifications.subscribe("action_policy.apply_rule") do |*args|
-  Rails.logger.debug do
-    event  = ActiveSupport::Notifications::Event.new(*args)
-    result = event.payload[:value] ? "granted" : "denied"
-    policy = event.payload[:policy]
-    rule   = event.payload[:rule]
-    cached = event.payload[:cached] ? "hit" : "missed"
-
-    "  \e[1m[ActionPolicy]\e[0m Apply #{result} for #{policy}##{rule} (cache #{cached})"
-  end
+Rails.application.configure do
+  config.action_policy.cache_store = Rails.cache
 end
 
-ActiveSupport::Notifications.subscribe("action_policy.authorize") do |*args|
-  Rails.logger.debug do
-    event  = ActiveSupport::Notifications::Event.new(*args)
-    result = event.payload[:value] ? "granted" : "denied"
-    policy = event.payload[:policy]
-    rule   = event.payload[:rule]
-    cached = event.payload[:cached] ? "hit" : "missed"
+# Add INSTRUMENT_ACTION_POLICY=true to your .env file to get log more
+# details about policies
+#
+if ENV.fetch("INSTRUMENT_ACTION_POLICY", "false") == "true"
+  ActiveSupport::Notifications.subscribe("action_policy.apply_rule") do |*args|
+    Rails.logger.debug do
+      event  = ActiveSupport::Notifications::Event.new(*args)
+      result = event.payload[:value] ? "granted" : "denied"
+      policy = event.payload[:policy]
+      rule   = event.payload[:rule]
+      cached = event.payload[:cached] ? "hit" : "missed"
 
-    "  \e[1m[ActionPolicy]\e[0m Authorize #{result} for #{policy}##{rule} (cache #{cached})"
+      if result == "denied"
+        reasons = event.payload[:result].reasons.details || {}
+        details = " (reasons: #{reasons})"
+      end
+
+      "  \e[1m[ActionPolicy]\e[0m Apply #{result} for #{policy}##{rule}#{details} (cache #{cached})"
+    end
+  end
+
+  ActiveSupport::Notifications.subscribe("action_policy.authorize") do |*args|
+    Rails.logger.debug do
+      event  = ActiveSupport::Notifications::Event.new(*args)
+      result = event.payload[:value] ? "granted" : "denied"
+      policy = event.payload[:policy]
+      rule   = event.payload[:rule]
+      cached = event.payload[:cached] ? "hit" : "missed"
+
+      "  \e[1m[ActionPolicy]\e[0m Authorize #{result} for #{policy}##{rule} (cache #{cached})"
+    end
   end
 end
